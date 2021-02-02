c08ef03845ce1416bce207922234866cf799cdd8
Gleb Pomykalov <gleb@exogress.com>
2021-02-03 02:13:25 +0300

    support command passing through env vars

--- features/step_definitions/exogress_cli.rb
+++ features/step_definitions/exogress_cli.rb
@@ -5,7 +5,15 @@ When(/^I spawn exogress client$/) do
   spawned = ConditionVariable.new
 
   Thread.new do
-      $exogress_cli_stdin, $exogress_cli_stdout, $exogress_cli_stderr, $exogress_cli_wait_thr = Open3.popen3("exogress spawn", :chdir => $scenario_dir)
+      cmd = ENV["COMMAND"] || "exogress"
+      env = {}
+
+      if ENV["CLOUD_ENDPOINT"]
+        env["CLOUD_ENDPOINT"] = ENV["CLOUD_ENDPOINT"]
+      end
+
+      $exogress_cli_stdin, $exogress_cli_stdout, $exogress_cli_stderr, $exogress_cli_wait_thr =
+        Open3.popen3(env, "#{cmd} spawn", :chdir => $scenario_dir)
       Thread.new do
         while line = $exogress_cli_stdout.gets do
           if line.include? "to signal server established"
@@ -14,17 +22,17 @@ When(/^I spawn exogress client$/) do
             end
           end
           if line.include? "ERROR"
-            Kernel.puts(line)
+            STDERR.puts(line)
           else
             if ENV["LOG_CLIENT"] == "1"
-              Kernel.puts(line)
+              STDERR.puts(line)
             end
           end
         end
       end
       Thread.new do
         while line = $exogress_cli_stderr.gets do
-          Kernel.puts(line)
+          STDERR.puts(line)
         end
       end
   end
