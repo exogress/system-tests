fa1f18a31849faa19c1fec639e5c9292734410f4
Gleb Pomykalov <gleb@exogress.com>
2021-02-03 19:12:49 +0300

    add profiles tests

--- features/step_definitions/exogress_cli.rb
+++ features/step_definitions/exogress_cli.rb
@@ -1,10 +1,24 @@
 require 'open3'
+require 'rspec/expectations'
 
-When(/^I spawn exogress client$/) do
-  mutex = Mutex.new
-  spawned = ConditionVariable.new
+class ExogressCli
+  include RSpec::Matchers
+  extend RSpec::Matchers
 
-  Thread.new do
+  @@process_lock = Mutex::new
+  @@pid = nil
+
+  def self.stop
+    @@process_lock.synchronize do
+      Process.kill("KILL", @@pid)
+    end
+  end
+
+  def self.spawn_cli(app_args)
+    mutex = Mutex.new
+    spawned = ConditionVariable.new
+
+    Thread.new do
       cmd = ENV["COMMAND"] || "exogress"
       env = {}
 
@@ -13,7 +27,12 @@ When(/^I spawn exogress client$/) do
       end
 
       $exogress_cli_stdin, $exogress_cli_stdout, $exogress_cli_stderr, $exogress_cli_wait_thr =
-        Open3.popen3(env, "#{cmd} spawn", :chdir => $scenario_dir)
+        Open3.popen3(env, "#{cmd} spawn #{app_args}", :chdir => $scenario_dir)
+
+      @@process_lock.synchronize do
+        @@pid = $exogress_cli_wait_thr[:pid]
+      end
+
       Thread.new do
         while line = $exogress_cli_stdout.gets do
           if line.include? "to signal server established"
@@ -35,9 +54,22 @@ When(/^I spawn exogress client$/) do
           STDERR.puts(line)
         end
       end
-  end
+    end
 
-  mutex.synchronize do
-    expect { spawned.wait(mutex) }.to_not raise_error, "client stopped before getting initialized"
+    mutex.synchronize do
+      expect { spawned.wait(mutex) }.to_not raise_error, "client stopped before getting initialized"
+    end
   end
+end
+
+When(/^I spawn exogress client$/) do
+  ExogressCli::spawn_cli("")
+end
+
+When(/^I spawn exogress client with profile "([^"]*)"$/) do |profile|
+  ExogressCli::spawn_cli("--profile=#{profile}")
+end
+
+When(/^I stop running exogress client$/) do
+  ExogressCli::stop
 end
